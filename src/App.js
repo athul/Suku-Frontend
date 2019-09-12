cf0838e0f150931fe36f0faf5308cba74fcd6144
athul <athul8720@gmail.com>
2019-09-13 00:23:09 +0530

    New Fixes added Axios

--- src/App.js
+++ src/App.js
@@ -1,32 +1,11 @@
 import React, {Component} from 'react';
-import User from './components/Userdata';
-//import Author from './components/author'
+//import UserData from './components/Userdata';
+import User from './components/user'
 class App extends Component {
     render() {
         return (
-            <User udatas={this.state.udatas} />
+          <User/>
                 )
-    }
-
-state = {
-        udatas: [],
-        //authors: []
-    };
-/*     state={
-
-    };
- */
-    componentDidMount() {
-        fetch('http://localhost:8000/API/ID/User/')
-            .then(res => res.json())
-            .then((data) => {
-                this.setState({ udatas: data })
-            })
-        /* fetch('https://59076f16-05fa-4104-b6e6-86d283c2e8c7.mock.pstmn.io/a')
-            .then(res => res.json())
-            .then((data) => {
-                this.setState({ authors: data })
-            }) */
     }
 }
 
